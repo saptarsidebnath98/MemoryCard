81faac0f3cb58837ea795726834305cad9a66c64
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-25 11:23:57 +0530

    main function  done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
     <header className="flex flex-col items-center md:flex-row md:justify-between px-4 gap-2">
        <h2 className="text-2xl font-bold font-mono">Memory Card Game</h2>
        <div className="flex gap-3 bg-slate-900 px-4 rounded-md text-xl text-stone-100 font-mono font-semibold">
-           <span>Score: 0</span>
-           <span>Best Score: 10</span>
+           <span className="flex flex-col md:flex-row text-center">Score: 0</span>
+           <span className="flex flex-col md:flex-row text-center">Best Score: 10</span>
        </div>
      </header>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import Dummy from "./Dummy";
 
 export default function Main() {
+  const [storeBtns, setStoreBtns] = useState([]);
+
+  let score = storeBtns.length;
+
+  const duplicateDummy = Dummy;
+
+  function handleClick(event){
+    if(!storeBtns.includes(event.target.name)){
+      setStoreBtns(prevBtns =>[...prevBtns, event.target.name])
+    }else{
+      setStoreBtns([]);
+    }
+  }
+
+    console.log("Score:", score);
+
   return (
-    <div>
-        
+    <div className="w-full flex md:flex-row gap-4 px-4 py-8 justify-center">
+        {duplicateDummy.map((btn)=>
+        <button 
+        key={btn.id} 
+        name={btn.name}
+        className="h-48 w-48 bg-stone-400 rounded-md"
+        onClick={handleClick}>
+          {btn.name}
+        </button>)}
     </div>
   )
 }
